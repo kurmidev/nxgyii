645e2775580a892d3a2c3c9051369c3702f06c0c
chandrapliquiloans <112621585+chandrapliquiloans@users.noreply.github.com>
2025-05-28 17:38:10 +0530

    feat: added access denied files

---
+++ views/site/accessdenied.php
@@ -0,0 +1,15 @@
+<div class="ht-10v d-flex align-items-center justify-content-center">
+    <div class="wd-lg-70p wd-xl-50p tx-center pd-x-40">
+        <h1 class="tx-100 tx-xs-140 tx-normal tx-inverse tx-roboto mg-b-0">403</h1>
+        <h5 class="tx-xs-24 tx-normal tx-info mg-b-30 lh-5">The page your are looking for has not been found.</h5>
+        <p class="tx-16 mg-b-30">The page you are looking for might have been removed, had its name changed,
+            or not permissable.</p>
+        <div class="d-flex justify-content-center">
+            <div class="input-group wd-xs-300">
+                <button class="btn btn-outline-teal btn-block mg-b-10">
+                    <a href="/">Go to Dashboad</a>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
