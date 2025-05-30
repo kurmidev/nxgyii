9aea9bfaf2e5317b9253b83b97fab6413a87ab8b
chandrapliquiloans <112621585+chandrapliquiloans@users.noreply.github.com>
2025-05-30 14:57:12 +0530

    fix: zzoho api url changes

--- component/Zoho.php
+++ component/Zoho.php
@@ -12,8 +12,8 @@ class Zoho
     public $client;
     public $token;
     public $refreshToken;
-    public $baseUrl = "https://desk.zoho.com/api/v1";
-
+    //public $baseUrl = "https://desk.zoho.com/api/v1";
+    public $baseUrl = "https://desk.zoho.in/api/v1";
     public $headers;
 
     public function __construct()
@@ -44,7 +44,7 @@ class Zoho
             'client_secret' => Yii::$app->params['zoho']["CLIENT_SECRET"],
         ];
         print_r($refreshToken);
-        $endPoint = "https://accounts.zoho.in/oauth/v2/token?refresh_token=$refreshToken&client_id=" . Yii::$app->params['zoho']["CLIENT_ID"] . "&client_secret=" . Yii::$app->params['zoho']["CLIENT_SECRET"] . "&scope=ZohoCRM.modules.ALL&grant_type=refresh_token";
+        $endPoint = "https://accounts.zoho.in/oauth/v2/token?refresh_token=$refreshToken&client_id=" . Yii::$app->params['zoho']["CLIENT_ID"] . "&client_secret=" . Yii::$app->params['zoho']["CLIENT_SECRET"] . "&scope=ZohoSupport.tickets.ALL&grant_type=refresh_token";
         echo $endPoint . PHP_EOL;
         $response = $this->getData($endPoint, "POST", []);
         echo "generating refresh token...." . PHP_EOL;
