7b01a6ea0ac1681606fad4fc6089602455514e3f
chandrapliquiloans <112621585+chandrapliquiloans@users.noreply.github.com>
2025-07-01 00:02:58 +0530

    fix: bug fix

--- views/complaint/complaint-reply.php
+++ views/complaint/complaint-reply.php
@@ -53,7 +53,7 @@ use PHPUnit\TextUI\Configuration\Constant;
             <div class="card-body d-flex justify-content flex-column p-8">
                 <a href="<?=Yii::$app->urlManager->createUrl(["site/download-file","id"=>$model->id])?>" class="text-gray-800 text-hover-primary d-flex flex-column">
                     <div class="symbol symbol-60px mb-5">
-                        <img class="theme-light-show" src="./media/svg/files/<?=!empty($attachment['extension'])?Constants::SHOW_MINE_IMAGES[$attachment['extension']]:""?>" alt="<?=!empty($attachment["name"])?$attachment["name"]:""?>" />
+                        <img class="theme-light-show" src="./media/svg/files/<?=!empty($attachment['extension']) && !empty(Constants::SHOW_MINE_IMAGES[$attachment['extension']])?Constants::SHOW_MINE_IMAGES[$attachment['extension']]:""?>" alt="<?=!empty($attachment["name"])?$attachment["name"]:""?>" />
                     </div>
                 </a>
             </div>
